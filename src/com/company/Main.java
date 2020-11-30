90f21186d607a1a729025e1814608cd900415905
aitorsp-elpuig <aitorsp@elpuig.xeill.net>
2020-11-30 22:36:30 +0100

    Upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,21 @@ package com.company;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    //7 Crea un _Reference Method_i utilitza'l per imprimir totes les persones de la Collection amb foreach
+    public static void imprimir(Persona p) {
+        System.out.println("***************************");
+        System.out.println(p);
+        System.out.println("***************************");
+    }
 
+    public static void main(String[] args) {
+        //1 Definir la variable format per instaciar els objectes del punt 2 (DateTimeFormatter)
         DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        //2 Crear
         Persona p1 = new Persona("Arya", Persona.Genere.DONA, LocalDate.parse("25/12/2002",format) );
         Persona p2 = new Persona("Tyrion", Persona.Genere.HOME, LocalDate.parse("12/10/1980",format));
         Persona p3 = new Persona("Cersei", Persona.Genere.DONA, LocalDate.parse("10/01/1984",format));
@@ -23,6 +29,7 @@ public class Main {
         Persona p10 = new Persona("Jon Neu", Persona.Genere.HOME, LocalDate.parse("12/11/1986",format));
         Persona p11 = new Persona("Brienne", Persona.Genere.DONA, LocalDate.parse("12/11/1989",format));
 
+        //3 Crear un ArrayList d'aquestes persones (llistaPersones)
         List<Persona> llistaPersones = new ArrayList<>();
         llistaPersones.add(p1);
         llistaPersones.add(p2);
@@ -36,16 +43,34 @@ public class Main {
         llistaPersones.add(p10);
         llistaPersones.add(p11);
 
-        // 4
+        //4 Canvia el següent codi per una expressió lambda:
+        /*/
+        Collections.sort(llistaPersones, new Comparator<Persona>() {
+            @Override
+            public int compare(Persona o1, Persona o2) {
+                if(o1.getNom().charAt(0) >= o2.getNom().charAt(0)) return 1;
+                else return -1;
+            }
+        });
+         */
         Collections.sort(llistaPersones, (o1, o2) -> {
             if (o1.getNom().charAt(0) >= o2.getNom().charAt(0)) return 1;
             else return -1;
             });
 
-        // 5
+        //5 Canvia el següent codi per una expressió lambda
+        /*/
+           for(Persona p: llistaPersones) {
+            System.out.println(p);
+           }
+         */
         llistaPersones.forEach(persona -> System.out.println(persona));
 
-        //6
+        //6 Transforma la lambda en una classe anònima
+        /*/
+        //ordenació alfabètica inversa del nom
+        llistaPersones.sort((o1,o2) -> o2.getNom().compareTo(o1.getNom()));
+         */
         llistaPersones.sort(new Comparator<Persona>() {
             @Override
             public int compare(Persona o1, Persona o2) {
@@ -53,18 +78,30 @@ public class Main {
             }
         });
 
-        // 7
-        //substituir pel foerech amb el reference method
-        llistaPersones.forEach(System.out::println);
+        //7 Crea un _Reference Method_i utilitza'l per imprimir totes les persones de la Collection amb foreach
+        /*/
+           //substituir pel foerech amb el reference method
+            for(Persona p: llistaPersones) {
+                System.out.println(p);
+            }
+         */
+        llistaPersones.forEach(Main::imprimir);
 
-        //8
+        //8 Crea un Map de Persona on la key és l'edat i el value és quantes perones hi ha amb aquesta edat. Esbrina com fer els puts al map usant aquests mètodes computeIfPresent i putIfAbsent
+        Map<Integer, Integer> mapPersones = new HashMap<>();
+        for (Persona persona : llistaPersones) {
+            if (!mapPersones.containsKey(persona.getAge())){
+                 mapPersones.put(persona.getAge(),1);
+            } else {
+                mapPersones.put(persona.getAge(), mapPersones.get(persona.getAge()) + 1);
+            }
+        }
+        //9 Fes un recorregut per imprimir clau i valor del map, amb foreach i una lambda o reference method.
 
-        //9
+        //10 Treu un llistat de les persones DONA. Cal fer servir STREAM.
 
-        //10
+        //11 Treu un llistat dels 2 HOMES més joves. Fer servir STREAM
 
-        //11
-
-        //12
+        //12 Esborrar del llistat les persones entre 30 i 40 anys (removeIf)
     }
 }
